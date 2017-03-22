4aac38a5121483babb8a7ae9a3ceb743bb00e79d
Balazs Szerencsi <balazs.szerencsi@icloud.com>
2017-03-22 07:58:05 +0100

    Removed failing dummy test

--- spec/pagerduty_rest_api_spec.rb
+++ spec/pagerduty_rest_api_spec.rb
@@ -6,8 +6,4 @@ RSpec.describe PagerdutyRestApi do
   it 'has a version number' do
     expect(PagerdutyRestApi::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
